54dd1f3ffa49336b3703e5dd7253ba596eeadb49
31anant <anantagrawal@in.ibm.com>
2020-10-15 12:29:13 +0000

    Lab 4

--- main.js
+++ main.js
@@ -35,7 +35,14 @@ const IPCIDR = require('ip-cidr');
 */
 class IpAddress {
   constructor() {
-/**
+    // IAP's global log object is used to output errors, warnings, and other
+    // information to the console, IAP's log files, or a Syslog server.
+    // For more information, consult the Log Class guide on the Itential
+    // Developer Hub https://developer.itential.io/ located
+    // under Documentation -> Developer Guides -> Log Class Guide
+    log.info('Starting the IpAddress product.');
+  }
+  /**
  * Calculate and return the first host IP address from a CIDR subnet.
  * @param {object} cidrObj - The IPv4 subnet expressed
  *                 in CIDR format.
@@ -43,11 +50,9 @@ class IpAddress {
  * @return {string} (firstIpAddress) - An IPv4 address.
  */
  getFirstIpAddress(cidrObj, callback) {
-
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
-
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrObj);
   // Initialize options for the toArray() method.
@@ -65,7 +70,7 @@ class IpAddress {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    IPv6Address = getIpv4MappedIpv6Address(firstIpAddress)
+    IPv6Address = getIpv4MappedIpv6Address(firstIpAddress);
   }
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
@@ -73,14 +78,6 @@ class IpAddress {
   // data as the second argument to the callback function.
   return callback({ipv4: firstIpAddress, ipv6: IPv6Address},callbackError);
 }
-
-    // IAP's global log object is used to output errors, warnings, and other
-    // information to the console, IAP's log files, or a Syslog server.
-    // For more information, consult the Log Class guide on the Itential
-    // Developer Hub https://developer.itential.io/ located
-    // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
-  }
 }
 
 module.exports = new IpAddress;
